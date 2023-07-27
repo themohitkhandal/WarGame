dbe266f9a2a03c28d1ff30dd1aae4520ec6e9d45
Mohit Khandal <mohit.khandal@anduriltechnologies.com>
2023-07-27 12:17:41 +0530

    Initial Commit

---
+++ Game/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by Anurag Bajpai on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
